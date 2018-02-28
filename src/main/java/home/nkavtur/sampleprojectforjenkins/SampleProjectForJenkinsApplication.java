89b08f1d607403247a0e543f1df2705bf761dc17
n.kavtur <n.kavtur@gmail.com>
2018-02-28 15:11:59 +0300

    update

---
+++ src/main/java/home/nkavtur/sampleprojectforjenkins/SampleProjectForJenkinsApplication.java
@@ -0,0 +1,12 @@
+package home.nkavtur.sampleprojectforjenkins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleProjectForJenkinsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleProjectForJenkinsApplication.class, args);
+	}
+}
