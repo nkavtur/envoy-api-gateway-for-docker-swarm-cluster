29b8009d7aa8f2ad559771af5f14f234b1012d7a
n.kavtur <n.kavtur@gmail.com>
2018-02-28 16:53:58 +0300

    add rest endpoint

--- src/main/java/home/nkavtur/sampleprojectforjenkins/SampleProjectForJenkinsApplication.java
+++ src/main/java/home/nkavtur/sampleprojectforjenkins/SampleProjectForJenkinsApplication.java
@@ -1,12 +1,25 @@
 package home.nkavtur.sampleprojectforjenkins;
 
+import lombok.SneakyThrows;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.net.InetAddress;
 
 @SpringBootApplication
+@RestController
 public class SampleProjectForJenkinsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SampleProjectForJenkinsApplication.class, args);
+	}
+
+
+	@GetMapping("/who-am-i")
+	@SneakyThrows
+	public String whoAmI() {
+		return InetAddress.getLocalHost().toString();
 	}
 }
