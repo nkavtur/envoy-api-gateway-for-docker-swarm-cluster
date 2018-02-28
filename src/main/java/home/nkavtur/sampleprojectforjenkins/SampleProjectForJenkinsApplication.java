de2488649d29aa680fd218c9e5b77800740d0af0
n.kavtur <n.kavtur@gmail.com>
2018-02-28 23:22:45 +0300

    update

--- src/main/java/home/nkavtur/sampleprojectforjenkins/SampleProjectForJenkinsApplication.java
+++ src/main/java/home/nkavtur/sampleprojectforjenkins/SampleProjectForJenkinsApplication.java
@@ -3,10 +3,13 @@ package home.nkavtur.sampleprojectforjenkins;
 import lombok.SneakyThrows;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.net.InetAddress;
+
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
 @SpringBootApplication
 @RestController
@@ -17,7 +20,7 @@ public class SampleProjectForJenkinsApplication {
 	}
 
 
-	@GetMapping("/who-am-i")
+	@GetMapping(value = "/who-am-i", produces = APPLICATION_JSON_VALUE)
 	@SneakyThrows
 	public String whoAmI() {
 		return InetAddress.getLocalHost().toString();
