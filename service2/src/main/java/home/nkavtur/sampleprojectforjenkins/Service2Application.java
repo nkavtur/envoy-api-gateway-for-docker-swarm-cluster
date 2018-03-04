3dfa443fb55294133d43c3e7506726a627c7eb29
n.kavtur <n.kavtur@gmail.com>
2018-03-04 23:55:13 +0300

    Add another service

--- service2/src/main/java/home/nkavtur/sampleprojectforjenkins/Service2Application.java
+++ service2/src/main/java/home/nkavtur/sampleprojectforjenkins/Service2Application.java
@@ -7,22 +7,23 @@ import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
 import java.net.InetAddress;
 
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
 @SpringBootApplication
 @RestController
-public class SampleProjectForJenkinsApplication {
+public class Service2Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SampleProjectForJenkinsApplication.class, args);
+		SpringApplication.run(Service2Application.class, args);
 	}
-
 
 	@GetMapping(value = "/who-am-i", produces = APPLICATION_JSON_VALUE)
 	@SneakyThrows
-	public String whoAmI() {
-		return InetAddress.getLocalHost().toString();
+	public String whoAmI(HttpServletRequest request) {
+		return String.format("%s %s", request.getContextPath(), InetAddress.getLocalHost().toString());
 	}
 }
+
---
+++ service2/src/test/java/home/nkavtur/sampleprojectforjenkins/SampleProjectForJenkinsApplicationTests.java
@@ -0,0 +1,44 @@
+package home.nkavtur.sampleprojectforjenkins;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import java.net.InetAddress;
+
+import static org.springframework.http.MediaType.APPLICATION_JSON;
+import static org.springframework.http.MediaType.APPLICATION_JSON_UTF8;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SampleProjectForJenkinsApplicationTests {
+
+
+	@Autowired
+	private WebApplicationContext wac;
+
+	private MockMvc mockMvc;
+
+	@Before
+	public void setup() {
+		this.mockMvc = MockMvcBuilders.webAppContextSetup(this.wac).build();
+	}
+
+	@Test
+	public void whoAmI() throws Exception {
+		mockMvc.perform(get("/who-am-i"))
+				.andExpect(status().isOk())
+				.andExpect(content().contentType(APPLICATION_JSON_UTF8))
+				.andExpect(jsonPath("$").value(InetAddress.getLocalHost().toString()));
+	}
+}
