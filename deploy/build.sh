a9f91682d0fe62d42b64a9120857ab2ead2bc8c5
n.kavtur <n.kavtur@gmail.com>
2018-03-05 10:48:02 +0300

    Update build.sh

--- deploy/build.sh
+++ deploy/build.sh
@@ -4,13 +4,18 @@ cd "$(dirname "$0")"
 echo "Building image front-proxy:latest ..."
 cd ../front-proxy
 docker build -t front-proxy:latest -f docker/Dockerfile .
-
+docker tag front-proxy:latest nkavtur/front-proxy:latest
+docker push nkavtur/front-proxy:latest
 
 echo "Building image service1:latest ..."
 cd ../service1
 docker build -t service1:latest -f docker/Dockerfile .
+docker tag service1:latest nkavtur/service1:latest
+docker push nkavtur/front-proxy:latest
 
 
 echo "Building image service2:latest ..."
 cd ../service2
 docker build -t service2:latest -f docker/Dockerfile .
+docker tag service2:latest nkavtur/service2:latest
+docker push nkavtur/service2:latest
