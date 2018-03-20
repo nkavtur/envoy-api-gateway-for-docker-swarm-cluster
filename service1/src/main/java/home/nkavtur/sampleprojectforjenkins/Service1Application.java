b3f02888838d3b71dbc04e4f5971a1a97b906931
n.kavtur <n.kavtur@gmail.com>
2018-03-21 00:55:37 +0300

    Add support for health checks

--- service1/src/main/java/home/nkavtur/sampleprojectforjenkins/Service1Application.java
+++ service1/src/main/java/home/nkavtur/sampleprojectforjenkins/Service1Application.java
@@ -3,12 +3,14 @@ package home.nkavtur.sampleprojectforjenkins;
 import lombok.SneakyThrows;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
 import javax.servlet.http.HttpServletRequest;
 import java.net.InetAddress;
+import java.util.function.Predicate;
 
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
@@ -20,6 +22,11 @@ public class Service1Application {
 		SpringApplication.run(Service1Application.class, args);
 	}
 
+	private static final String OK = "Ok";
+	private static final String FAIL = "Fail";
+
+	private boolean healthy = true;
+
 	private RestTemplate restTemplate = new RestTemplate();
 
 	@GetMapping(value = "/call-service2", produces = APPLICATION_JSON_VALUE)
@@ -28,11 +35,21 @@ public class Service1Application {
 		return restTemplate.getForObject("http://service2:9090/service2/who-am-i", String.class);
 	}
 
-
 	@GetMapping(value = "/who-am-i", produces = APPLICATION_JSON_VALUE)
 	@SneakyThrows
 	public String whoAmI(HttpServletRequest request) {
-		System.out.println(" I SERVICE1 AM HERE!");
 		return String.format("%s %s", request.getContextPath(), InetAddress.getLocalHost().toString());
 	}
+
+	@GetMapping(value = "/healthcheck", produces = APPLICATION_JSON_VALUE)
+	public ResponseEntity<String> healthcheck() {
+		System.out.println("HEALTH CHECK CALL!!!!!!!!!!!!!!!!!");
+		return healthy ? ResponseEntity.ok(OK) : ResponseEntity.status(503).body(FAIL);
+	}
+
+	@PostMapping(value = "/healthcheck", consumes = MediaType.APPLICATION_JSON_VALUE)
+	public void healthcheck(@RequestBody Boolean healthy) {
+		this.healthy = healthy;
+	}
+
 }
--- service2/src/main/java/home/nkavtur/sampleprojectforjenkins/Service2Application.java
+++ service2/src/main/java/home/nkavtur/sampleprojectforjenkins/Service2Application.java
@@ -4,7 +4,10 @@ import lombok.SneakyThrows;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
@@ -20,11 +23,27 @@ public class Service2Application {
 		SpringApplication.run(Service2Application.class, args);
 	}
 
+	private static final String OK = "Ok";
+	private static final String FAIL = "Fail";
+
+	private boolean healthy = true;
+
 	@GetMapping(value = "/who-am-i", produces = APPLICATION_JSON_VALUE)
 	@SneakyThrows
 	public String whoAmI(HttpServletRequest request) {
 		System.out.println(" I SERVICE2 AM HERE!");
 		return String.format("%s %s", request.getContextPath(), InetAddress.getLocalHost().toString());
 	}
+
+	@GetMapping(value = "/healthcheck", produces = APPLICATION_JSON_VALUE)
+	public ResponseEntity<String> healthcheck() {
+		return healthy ? ResponseEntity.ok(OK) : ResponseEntity.status(503).body(FAIL);
+	}
+
+	@PostMapping(value = "/healthcheck", consumes = MediaType.APPLICATION_JSON_VALUE)
+	public void healthcheck(@RequestBody Boolean healthy) {
+		this.healthy = healthy;
+	}
+
 }
 
